671be0c16811029f8d61c88fef088e41cf193fb4
Loz Jackson <lozjackson@users.noreply.github.com>
2022-04-04 19:58:06 +0100

    export a function to be used in the router
    replace `this.authenticatedRoute(...args)` with `authenticatedRoute(this, ...args)`

--- addon/router-dsl-ext.js
+++ addon/router-dsl-ext.js
@@ -1,14 +1,12 @@
-import Ember from 'ember';
 import Router from '@ember/routing/router';
 import getRouterLib from 'torii/compat/get-router-lib';
 
-var proto = Ember.RouterDSL.prototype;
-
 var currentMap = null;
 
-proto.authenticatedRoute = function() {
-  this.route.apply(this, arguments);
-  currentMap.push(arguments[0]);
+export function authenticatedRoute(...args) {
+  const router = args.shift()
+  router.route.apply(router, args);
+  currentMap.push(args[0]);
 };
 
 Router.reopen({
